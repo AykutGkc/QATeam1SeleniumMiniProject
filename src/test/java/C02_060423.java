469740e9ceb12294cf5900e48387e77cc83aa9d7
Gülsüm Sevil Yavuz <gulsumyavuz54@gmail.com>
2023-04-07 18:36:40 +0200

    degisiklik

--- src/test/java/C02_060423.java
+++ src/test/java/C02_060423.java
@@ -1,6 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.TestBase;
 
-public class C02_060423 {
+import java.time.Duration;
+
+
+public class C02_060423 extends TestBase {
     /*
     --Utilities package'da TestBase class'i olusturuldu. Orada ki methodlar incelenip kullanilabilir. 
     --@Test ile Junit Framework'ünde  testlerimizi yapacagiz.
@@ -304,19 +313,10 @@ public class C02_060423 {
 
 
 
-
-
-
-
-
-
-
-
-
     }
 
     @Test
-    public void sevilTest() { //318-356
+    public void sevilTest() throws InterruptedException { //318-356
         /*
         a. Verilen web sayfasına gidin.
         https://the-internet.herokuapp.com/checkboxes
@@ -324,24 +324,24 @@ public class C02_060423 {
         c. Checkbox1 seçili değilse onay kutusunu tıklayın
         d. Checkbox2 seçili değilse onay kutusunu tıklayın
          */
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        Thread.sleep(3000);
+        WebElement checkout1 = driver.findElement(By.xpath("// input [@type='checkbox'][1]"));
+        WebElement checkout2 = driver.findElement(By.xpath("// input [@type='checkbox'][2]"));
+        if (!checkout1.isSelected()) {
+            checkout1.click();
+        }
+        if (!checkout2.isSelected()) {
+            checkout2.click();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
 
 
