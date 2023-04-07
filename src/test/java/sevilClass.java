70fd4fc48f0c3be7aecc269a860a0ee337fc9229
Gülsüm Sevil Yavuz <gulsumyavuz54@gmail.com>
2023-04-07 17:17:56 +0200

    degisiklik

--- src/test/java/sevilClass.java
+++ src/test/java/sevilClass.java
@@ -1,6 +1,13 @@
+import org.junit.Test;
 public class sevilClass {
 
-    public static void main(String[] args) {
-        System.out.println("sevil bunu yazdi");
-    }
-}
+@Test
+public void sevilTest() { //318-356
+        /*
+        a. Verilen web sayfasına gidin.
+        https://the-internet.herokuapp.com/checkboxes
+        b. Checkbox1 ve checkbox2 elementlerini locate edin.
+        c. Checkbox1 seçili değilse onay kutusunu tıklayın
+        d. Checkbox2 seçili değilse onay kutusunu tıklayın
+         */
+}}
