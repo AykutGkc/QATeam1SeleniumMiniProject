a571a24e9662407e69b6692710c8fc946264e7c2
Gülsüm Sevil Yavuz <gulsumyavuz54@gmail.com>
2023-04-07 17:49:04 +0200

    degisikli

--- src/test/java/sevilClass.java
+++ src/test/java/sevilClass.java
@@ -1,8 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class sevilClass {
 
-@Test
-public void sevilTest() { //318-356
+    @Test
+    public void sevilTest() throws InterruptedException { //318-356
         /*
         a. Verilen web sayfasına gidin.
         https://the-internet.herokuapp.com/checkboxes
@@ -10,4 +18,21 @@ public void sevilTest() { //318-356
         c. Checkbox1 seçili değilse onay kutusunu tıklayın
         d. Checkbox2 seçili değilse onay kutusunu tıklayın
          */
-}}
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        driver.get("https://the-internet.herokuapp.com/checkboxes");
+        Thread.sleep(3000);
+        WebElement checkout1 = driver.findElement(By.xpath("// input [@type='checkbox'][1]"));
+        WebElement checkout2 = driver.findElement(By.xpath("// input [@type='checkbox'][2]"));
+        if (!checkout1.isSelected()) {
+            checkout1.click();
+        }
+        if (!checkout2.isSelected()) {
+            checkout2.click();
+        }
+    }
+}
