3dadc621e5bb353d9df75cc3732e1320118f0137
Aykut Gökce <aykutgokce2016@gmail.com>
2023-04-08 18:38:14 +0200

    v2

--- src/test/java/C02_060423.java
+++ src/test/java/C02_060423.java
@@ -1,13 +1,16 @@
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import utilities.TestBase;
 
 import java.time.Duration;
+import java.util.List;
 
 public class C02_060423 extends TestBase {
     /*
